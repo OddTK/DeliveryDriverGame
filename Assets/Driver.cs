74e61c8309ac1adaa1a0f750ca27b8ff00196474
Jesus <jesusbalandra@outlook.com>
2022-05-05 00:56:25 -0600

    created collision

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField] float steerSpeed = 1f;
-    [SerializeField] float moveSpeed = 0.01f;
+    [SerializeField] float steerSpeed = 300f;
+    [SerializeField] float moveSpeed = 20f;
     void Start()
     {
 
